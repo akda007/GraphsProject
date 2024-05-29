740b1a41d1e54e3b5f864ec9577d4fbf54858719
akda007 <akdagmr007s@gmail.com>
2024-05-28 21:41:07 -0300

    Created structs

--- include/dtypes.h
+++ include/dtypes.h
@@ -3,10 +3,23 @@
 
 typedef struct actors {
     int id;
+	char* primaryName;
+	int birthYear;
+	int deathYear;
+	char* primaryProfession;
+	//knownForTitles
 } actors_t;
 
 typedef struct movies {
-    
+    int id;
+	char* titleType;
+	char* primaryTitle;
+	char* originalTitle;
+	int isAdult;
+	int startYear;
+	int endYear;
+	int runtimeMinutes;
+	char* genres;
 } movies_t;
 
 #endif
